8d751785b50788ec9bfb72bafa6360cae31a7624
Akshaya Kumar <46162987+AkshayaKumar2311@users.noreply.github.com>
2021-04-02 00:31:37 +1100

    Update a2_31021301_task3.py
    Co-authored-by: Namrata Palai <namratapalai@gmail.com>

--- a2_31021301_task3.py
+++ a2_31021301_task3.py
@@ -3,7 +3,7 @@
 # Assignment 2 - FIT9136
 # Student ID : 31021301
 # Created on Fri May 27 2020
-# Author: Akshaya Kumar Chandrasekaran
+# Author: Akshaya Kumar Chandrasekaran / Co_author : Namrata Palai
 # Last modified : 04-06-2020
 #
 #Importing from task3
